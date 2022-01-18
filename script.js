61221541864d710610dff69c0c71766e9e6c52a6
allan <allan.riberio@gmail.com>
2022-01-18 16:54:04 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+let nome = `allan`
+console.log(nome)
+
+let number = 3
+console.log(number)
+
+
+let resultado = 2022 - number
+console.log(resultado)
+
+
+console.log(resultado + number)
+
+console.log("estou mexendo no console.log")
+
+let quartaFeira = true
+console.log(quartaFeira)
+
+
+let type = `Boolean`
+console.log(type)
